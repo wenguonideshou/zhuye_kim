a91f74ebac405be717114fb6df44a3ee5ad3993e
yetist <yetist@gmail.com>
2018-10-19 16:55:14 +0800

    support django 1.8+

--- zhuye_kim/urls.py
+++ zhuye_kim/urls.py
@@ -1,12 +1,26 @@
-from django.urls import path, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+import django
+if django.VERSION >= (2, 0):
+    from django.urls import path, include
+else:
+    from django.conf.urls import url, include
 
+if django.VERSION >= (1, 9):
+    web_url = include(("web.urls", "web"), namespace="web")
+else:
+    web_url = include("web.urls", "web", "web")
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(("web.urls", "web"), namespace="web")),
-]
+if django.VERSION >= (2, 0):
+    urlpatterns = [
+            path('admin/', admin.site.urls),
+            path('', web_url),
+    ]
+else:
+    urlpatterns = [
+            url(r'^admin/', include(admin.site.urls)),
+            url(r'^', web_url),
+    ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
